b5c54b130ceda3a50970827aa5b0793a86e37321
ShenyiCui <shenyicui@outlook.com>
2022-04-03 18:36:30 +0800

    Add PS7_Part3

--- Problem Sets/ps7_part2_v2/src/MazeSolverWithPower.java
+++ Problem Sets/ps7_part2_v2/src/MazeSolverWithPower.java
@@ -220,8 +220,25 @@ public class MazeSolverWithPower implements IMazeSolverWithPower {
 //			MazePrinter.printMaze(maze);
 //			System.out.println(solver.pathSearch(1, 2, 3, 2, 6)); // --> 7
 //			MazePrinter.printMaze(maze);
-			System.out.println(solver.pathSearch(0, 0, 0, 0, 3)); // --> 7
-			MazePrinter.printMaze(maze);
+			System.out.println(solver.pathSearch(0, 0, 0, 0, 3));
+			System.out.println(solver.pathSearch(0, 0, 0, 1, 3));
+			System.out.println(solver.pathSearch(0, 0, 0, 2, 3));
+			System.out.println(solver.pathSearch(0, 0, 0, 3, 3));
+
+			System.out.println(solver.pathSearch(0, 0, 1, 0, 3));
+			System.out.println(solver.pathSearch(0, 0, 1, 1, 3));
+			System.out.println(solver.pathSearch(0, 0, 1, 2, 3));
+			System.out.println(solver.pathSearch(0, 0, 1, 3, 3));
+
+			System.out.println(solver.pathSearch(0, 0, 2, 0, 3));
+			System.out.println(solver.pathSearch(0, 0, 2, 1, 3));
+			System.out.println(solver.pathSearch(0, 0, 2, 2, 3));
+			System.out.println(solver.pathSearch(0, 0, 2, 3, 3));
+
+			System.out.println(solver.pathSearch(0, 0, 3, 0, 3));
+			System.out.println(solver.pathSearch(0, 0, 3, 1, 3));
+			System.out.println(solver.pathSearch(0, 0, 3, 2, 3));
+			System.out.println(solver.pathSearch(0, 0, 3, 3, 3));
 //			System.out.println(solver.pathSearch(1, 1, 0, 0, 1000)); // --> 7
 //			MazePrinter.printMaze(maze);
 
